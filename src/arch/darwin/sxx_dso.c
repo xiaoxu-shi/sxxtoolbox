728b8a644f623d420dc314262be908fac01ed802
xiaoxu.shi <shixiaoxuyx@163.com>
2021-10-28 20:39:22 +0800

    fix: use sxx_declare on dso

--- src/arch/darwin/sxx_dso.c
+++ src/arch/darwin/sxx_dso.c
@@ -1,16 +1,16 @@
 #include "sxx_arch_dso.h"
 
-sxx_status_t sxx_dso_load(sxx_dso_handle_t **h, const char *path)
+SXX_DECLARE(sxx_status_t) sxx_dso_load(sxx_dso_handle_t **h, const char *path)
 {
     return SXX_ERROR;
 }
 
-sxx_status_t sxx_dso_sym(sxx_dso_handle_t *h, const char *symname, sxx_dso_handle_sym_t *sym)
+SXX_DECLARE(sxx_status_t) sxx_dso_sym(sxx_dso_handle_t *h, const char *symname, sxx_dso_handle_sym_t *sym)
 {
     return SXX_ERROR;
 }
 
-sxx_status_t sxx_dso_unload(sxx_dso_handle_t *h)
+SXX_DECLARE(sxx_status_t) sxx_dso_unload(sxx_dso_handle_t *h)
 {
     return SXX_ERROR;
 }
