0a83347e38f2b366ce3031e32b7d03b8938dd893
xiaoxu-shi <shixiaoxuyx@163.com>
2021-11-11 19:31:56 +0800

    feature: finish test framework
    1、sxx_test_framework_suite_add
    2、sxx_test_framework_run
    3、sxx_test_suite_create

--- src/sxx_test.c
+++ src/sxx_test.c
@@ -9,7 +9,6 @@ struct sxx_test_suite_t {
 	sxx_ptr_t   obj;
     sxx_test_f  tester;
 };
-typedef struct sxx_test_suite_t sxx_test_suite_t;
 
 struct sxx_test_list_t {
     SXX_LIST_MEMBER;
@@ -22,14 +21,27 @@ struct sxx_test_framework_t
     sxx_memory_pool_t*  pool;
     sxx_test_list_t     list;
 };
-typedef struct sxx_test_framework_t sxx_test_framework_t;
 
 
 SXX_DECLARE(sxx_test_suite_t*)  sxx_test_suite_create(
     sxx_memory_pool_t *pool, const sxx_char_t *name,
     sxx_ptr_t obj, sxx_test_f tester)
 {
-    return NULL;
+    sxx_assert(pool);
+    sxx_assert(tester);
+
+    sxx_test_suite_t* suite = NULL;
+
+    suite = sxx_memory_calloc(pool, sxx_sizeof(sxx_test_suite_t));
+    sxx_assert(suite);
+    suite->pool = pool;
+    suite->obj = obj;
+    suite->tester = tester;
+    if (sxx_string_assign_1(pool, &suite->name, (sxx_char_t*)name) == NULL) {
+        return NULL;
+    }
+
+    return suite;
 }
 
 
@@ -68,13 +80,38 @@ SXX_DECLARE(sxx_void_t) sxx_test_framework_destroy(sxx_test_framework_t *framewo
 
 SXX_DECLARE(sxx_bool_t) sxx_test_framework_suite_add(sxx_test_framework_t *framework, sxx_test_suite_t *suite)
 {
-    return SXX_ERROR;
+    sxx_assert(framework);
+    sxx_assert(suite);
+    sxx_test_list_t* node = NULL;
+
+    node = sxx_memory_alloc(framework->pool, sxx_sizeof(sxx_test_list_t));
+    sxx_assert(node);
+    if (node) {
+        return SXX_ERROR;
+    }
+
+    sxx_list_push_back(&framework->list, node);
+
+    return SXX_SUCCESS;
 }
 
 
 SXX_DECLARE(sxx_bool_t) sxx_test_framework_run(sxx_test_framework_t *framework, sxx_int32_t argc, sxx_char_t* argv[])
 {
-    return SXX_ERROR;
+    sxx_assert(framework);
+
+    sxx_list_ptr_t it = framework->list.next;
+    while (it != SXX_LIST_OBJECT_PTR(&framework->list)) {
+        sxx_test_list_t* node = (sxx_test_list_t *)it;
+        if (node->sutite && node->sutite->tester) {
+            if (node->sutite->tester(node->sutite, argc, argv) != SXX_TRUE) {
+                return SXX_ERROR;
+            }
+        }
+        it = it->next;
+    }
+
+    return SXX_SUCCESS;
 }
 
 
