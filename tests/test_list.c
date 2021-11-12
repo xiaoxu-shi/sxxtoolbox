31ba3d6273d14c8ffe00eee2767d5afe64762da5
xiaoxu-shi <shixiaoxuyx@163.com>
2021-11-12 11:05:14 +0800

    test: add list insert after

--- tests/test_list.c
+++ tests/test_list.c
@@ -8,7 +8,7 @@ typedef struct sxx_list_tester_node_t sxx_list_tester_node_t;
 
 static sxx_bool_t sxx_tester_list_empty(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 static sxx_bool_t sxx_tester_list_insert_before(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
-
+static sxx_bool_t sxx_tester_list_insert_after(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 
 
 sxx_bool_t sxx_tester_list(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_t *argv[])
@@ -17,7 +17,11 @@ sxx_bool_t sxx_tester_list(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_
         return SXX_FALSE;
     }
 
-    if (sxx_tester_list_insert_before(suite, argc, argv)) {
+    if (sxx_tester_list_insert_before(suite, argc, argv) != SXX_TRUE) {
+        return SXX_FALSE;
+    }
+
+    if (sxx_tester_list_insert_after(suite, argc, argv) != SXX_TRUE) {
         return SXX_FALSE;
     }
 
@@ -68,6 +72,36 @@ static sxx_bool_t sxx_tester_list_insert_before(sxx_test_suite_t* suite, sxx_int
         }
         it = it->next;
         index--;
+    }
+
+    return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_list_insert_after(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]) {
+    sxx_list_tester_node_t list;
+    sxx_list_init(&list);
+
+    sxx_list_tester_node_t* node0 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    node0->value = 0;
+    sxx_list_tester_node_t* node1 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    node1->value = 1;
+    sxx_list_tester_node_t* node2 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    node2->value = 2;
+
+    sxx_list_insert_after(&list, node0);
+    sxx_list_insert_after(node0, node1);
+    sxx_list_insert_after(node1, node2);
+
+    sxx_int32_t index = 0;
+    sxx_list_ptr_t it = list.next;
+    while (it != SXX_LIST_OBJECT_PTR(&list)) {
+        sxx_list_tester_node_t* n = (sxx_list_tester_node_t*)it;
+        //sxx_printf("%d\n", n->value);
+        if (n->value != index) {
+            return SXX_FALSE;
+        }
+        it = it->next;
+        index++;
     }
 
     return SXX_TRUE;
