fbe04aaffbd4aad79a9a024adfbbaec539aac85c
xiaoxu-shi <shixiaoxuyx@163.com>
2021-11-12 11:46:35 +0800

    test: add list insert nodes before

--- tests/test_list.c
+++ tests/test_list.c
@@ -9,7 +9,7 @@ typedef struct sxx_list_tester_node_t sxx_list_tester_node_t;
 static sxx_bool_t sxx_tester_list_empty(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 static sxx_bool_t sxx_tester_list_insert_before(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 static sxx_bool_t sxx_tester_list_insert_after(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
-
+static sxx_bool_t sxx_tester_list_insert_nodes_before(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 
 sxx_bool_t sxx_tester_list(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_t *argv[])
 {
@@ -25,8 +25,13 @@ sxx_bool_t sxx_tester_list(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_
         return SXX_FALSE;
     }
 
+    if (sxx_tester_list_insert_nodes_before(suite, argc, argv) != SXX_TRUE) {
+        return SXX_FALSE;
+    }
+
     return SXX_TRUE;
 }
+
 
 static sxx_bool_t sxx_tester_list_empty(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]) {
     sxx_list_tester_node_t list;
@@ -106,3 +111,41 @@ static sxx_bool_t sxx_tester_list_insert_after(sxx_test_suite_t* suite, sxx_int3
 
     return SXX_TRUE;
 }
+
+static sxx_bool_t sxx_tester_list_insert_nodes_before(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]) {
+    sxx_list_tester_node_t* list = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+
+    sxx_list_tester_node_t* node = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    sxx_list_tester_node_t* nodes0 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    sxx_list_tester_node_t* nodes1 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+    sxx_list_tester_node_t* nodes2 = sxx_memory_alloc(sxx_test_suite_pool_get(suite), sxx_sizeof(sxx_list_tester_node_t));
+
+    node->value = 3;
+    nodes0->value = 0;
+    nodes1->value = 1;
+    nodes2->value = 2;
+
+    sxx_list_init(list);
+    sxx_list_insert_after(list, node);
+
+    sxx_list_init(nodes0);
+    sxx_list_insert_after(nodes0, nodes1);
+    sxx_list_insert_after(nodes1, nodes2);
+
+    sxx_list_insert_nodes_before(node, nodes0);
+
+    sxx_int32_t index = 0;
+    sxx_list_ptr_t it = list->next;
+    while (it != SXX_LIST_OBJECT_PTR(list)) {
+        sxx_list_tester_node_t* n = (sxx_list_tester_node_t*)it;
+        //sxx_printf("%d\n", n->value);
+        if (n->value != index) {
+            return SXX_FALSE;
+        }
+        it = it->next;
+        index++;
+    }
+
+    return SXX_TRUE;
+}
+
