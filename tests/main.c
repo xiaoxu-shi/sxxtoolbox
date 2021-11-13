e0d45d8f9ae77552eb220c93bccbf7dbc02928b4
xiaoxu-shi <shixiaoxuyx@163.com>
2021-11-13 18:15:31 +0800

    test: add atomic

--- tests/main.c
+++ tests/main.c
@@ -2,16 +2,19 @@
 #include <sxx_toolbox.h>
 
 sxx_bool_t sxx_tester_list(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_t *argv[]);
+sxx_bool_t sxx_tester_atomic(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
 sxx_bool_t sxx_tester_memory_pool(sxx_test_suite_t *suite, sxx_int32_t argc,  sxx_char_t *argv[]);
 
 int main(int argc, char* argv[]) {
     sxx_test_framework_t *framework = sxx_test_framework_create();
     sxx_memory_pool_t *pool = sxx_test_framework_pool_get(framework);
 
-    sxx_test_suite_t *list = sxx_test_suite_create(pool, "list_tester", NULL, sxx_tester_list);
-    sxx_test_suite_t *mpool = sxx_test_suite_create(pool, "mpool_tester", NULL, sxx_tester_memory_pool);
+    sxx_test_suite_t *list = sxx_test_suite_create(pool, "tester_list", NULL, sxx_tester_list);
+    sxx_test_suite_t* atomic = sxx_test_suite_create(pool, "tester_atomic", NULL, sxx_tester_atomic);
+    sxx_test_suite_t *mpool = sxx_test_suite_create(pool, "tester_memory_pool", NULL, sxx_tester_memory_pool);
 
     sxx_test_framework_suite_add(framework, list);
+    sxx_test_framework_suite_add(framework, atomic);
     sxx_test_framework_suite_add(framework, mpool);
 
     sxx_test_framework_run(framework, argc, argv);
---
+++ tests/test_atomic.c
@@ -0,0 +1,112 @@
+#include <sxx_toolbox.h>
+
+static sxx_bool_t sxx_tester_atomic_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_set(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_inc(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_dec(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_add(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_inc_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_dec_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+static sxx_bool_t sxx_tester_atomic_add_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[]);
+
+sxx_bool_t sxx_tester_atomic(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_assert_return_val(sxx_tester_atomic_get(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_set(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_inc(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_dec(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_add(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_inc_and_get(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_dec_and_get(suite, argc, argv), SXX_FALSE);
+	sxx_assert_return_val(sxx_tester_atomic_add_and_get(suite, argc, argv), SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 1);
+
+	sxx_assert_return_val(sxx_atomic_get(atomic) == 1, SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_set(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 0);
+
+	sxx_atomic_set(atomic, 100);
+
+	sxx_assert_return_val(sxx_atomic_get(atomic) == 100, SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_inc(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 0);
+
+	sxx_atomic_inc(atomic);
+
+	sxx_assert_return_val(sxx_atomic_get(atomic) == 1, SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_dec(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 1);
+
+	sxx_atomic_dec(atomic);
+
+	sxx_assert_return_val(sxx_atomic_get(atomic) == 0, SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_add(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 0);
+
+	sxx_atomic_add(atomic, 100);
+
+	sxx_assert_return_val(sxx_atomic_get(atomic) == 100, SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_inc_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 0);
+
+	sxx_int64_t value = sxx_atomic_inc_and_get(atomic);
+
+	sxx_assert_return_val((sxx_atomic_get(atomic) == value == 1), SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_dec_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 1);
+
+	sxx_int64_t value = sxx_atomic_dec_and_get(atomic);
+
+	sxx_assert_return_val((sxx_atomic_get(atomic) == value && value == 0), SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+static sxx_bool_t sxx_tester_atomic_add_and_get(sxx_test_suite_t* suite, sxx_int32_t argc, sxx_char_t* argv[])
+{
+	sxx_atomic_t* atomic = sxx_atomic_create(sxx_test_suite_pool_get(suite), 0);
+
+	sxx_int64_t value = sxx_atomic_add_and_get(atomic, 100);
+
+	sxx_assert_return_val((sxx_atomic_get(atomic) == value && value == 100), SXX_FALSE);
+
+	return SXX_TRUE;
+}
+
+
