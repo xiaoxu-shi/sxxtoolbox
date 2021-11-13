5c7a6aaa3b5d1f694e35b61a524727cdb03df583
xiaoxu.shi <shixiaoxuyx@163.com>
2021-11-14 00:28:01 +0800

    feature: finish sxx_string_t

--- include/toolbox/sxx_cstr.h
+++ include/toolbox/sxx_cstr.h
@@ -1,13 +1,14 @@
 #ifndef __SXX_CSTR_H__
 #define __SXX_CSTR_H__
+#include <string.h>
 #include "toolbox/sxx_defs.h"
 #include "toolbox/sxx_types.h"
 
 SXX_EXTERN_C_BEGIN
 
-#define sxx_sizeof  sizeof
-#define sxx_strlen  strlen
-#define sxx_printf  printf
+#define sxx_sizeof(b)       sizeof(b)
+#define sxx_strlen(s)       strlen(s)
+#define sxx_strncpy(d,s,n)  strncpy(d,s,n)
 
 SXX_EXTERN_C_END
 
--- src/sxx_string.c
+++ src/sxx_string.c
@@ -1,4 +1,6 @@
+#include "sxx_cstr.h"
 #include "sxx_string.h"
+
 
 SXX_DECLARE(sxx_bool_t) sxx_string_empty(sxx_string_t *str)
 {
@@ -17,32 +19,50 @@ SXX_DECLARE(sxx_char_t*) sxx_string_buffer(sxx_string_t *str)
 
 SXX_DECLARE(sxx_string_t*) sxx_string_set(sxx_string_t* dst, sxx_string_t* src)
 {
+    dst->len = src->len;
+    dst->buf = src->buf;
     return dst;
 }
 
 SXX_DECLARE(sxx_string_t*) sxx_string_set_cstr(sxx_string_t* dst, sxx_char_t* csrc)
 {
+    dst->len = sxx_strlen(csrc);
+    dst->buf = csrc;
     return dst;
 }
 
 SXX_DECLARE(sxx_string_t*) sxx_string_assign(sxx_memory_pool_t* pool, sxx_string_t* dst, sxx_string_t* src)
 {
+    dst->len = src->len;
+    dst->buf = sxx_memory_calloc(pool, src->len + 1);
+    sxx_strncpy(dst->buf, src->buf, dst->len);
     return dst;
 }
 
 SXX_DECLARE(sxx_string_t*) sxx_string_assign_cstr(sxx_memory_pool_t* pool, sxx_string_t* dst, sxx_char_t* csrc)
 {
+    dst->len = sxx_strlen(csrc);
+    dst->buf = sxx_memory_calloc(pool, dst->len + 1);
+    sxx_strncpy(dst->buf, csrc, dst->len);
     return dst;
 }
 
 SXX_DECLARE(sxx_string_t*) sxx_string_dup(sxx_memory_pool_t* pool, sxx_string_t* str)
 {
-    return NULL;
+    sxx_string_t *dst = sxx_memory_alloc(pool, sxx_sizeof(sxx_string_t));
+    dst->buf = sxx_memory_calloc(pool, str->len + 1);
+    dst->len = str->len;
+    sxx_strncpy(dst->buf, str->buf, dst->len);
+    return dst;
 }
 
 SXX_DECLARE(sxx_string_t*) sxx_string_dup_cstr(sxx_memory_pool_t* pool, sxx_char_t* csrc)
 {
-    return NULL;
+    sxx_string_t *dst = sxx_memory_alloc(pool, sxx_sizeof(sxx_string_t));
+    dst->buf = sxx_memory_calloc(pool, sxx_strlen(csrc) + 1);
+    dst->len = sxx_strlen(csrc);
+    sxx_strncpy(dst->buf, csrc, dst->len);
+    return dst;
 }
 
 
