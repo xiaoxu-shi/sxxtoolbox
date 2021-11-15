229a1101b1eaed5ebdb36739466e6ee86bfdaf7a
xiaoxu.shi <shixiaoxuyx@163.com>
2021-11-16 00:15:26 +0800

    test: sxx_dso

--- tests/test_dso.c
+++ tests/test_dso.c
@@ -1,7 +1,16 @@
 #include "sxx_toolbox.h"
 
+typedef sxx_int32_t (*sxx_test_dso_add_f)(sxx_int32_t n1, sxx_int32_t n2);
+
 sxx_bool_t sxx_tester_dso(sxx_test_suite_t *suite, sxx_int32_t argc, sxx_char_t *argv[])
 {
+    sxx_test_dso_add_f add_f = NULL;
+
+    sxx_dso_handle_t *dh = sxx_dso_create(sxx_test_suite_pool_get(suite), "./libsxxtestdso.dylib");
+
+    sxx_assert_return_val(sxx_dso_sym(dh, "sxx_test_dso_add", (sxx_dso_sym_t*)&add_f) == SXX_SUCCESS, SXX_FALSE);
+
+    sxx_assert_return_val(add_f(1, 2) == 3, SXX_FALSE);
 
     return SXX_TRUE;
 }
