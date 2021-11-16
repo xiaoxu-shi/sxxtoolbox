7bd16494e48670b7cfc886065a83c889c067a65b
xiaoxu-shi <shixiaoxuyx@163.com>
2021-11-16 14:20:04 +0800

    refactor: directory structure

--- tests/test_dso.c
+++ tests/test_dso.c
@@ -1,5 +1,17 @@
 #include "sxx_toolbox.h"
 
+#ifdef _WIN32
+static sxx_const_char_t* dlpath = "./sxxtestdso.dll";
+#elif __APPLE__
+static sxx_const_char_t* dlpath = "./libsxxtestdso.dylib";
+#elif __linux__
+static sxx_const_char_t* dlpath = "./libsxxtestdso.so";
+#elif __unix__
+static sxx_const_char_t* dlpath = "./libsxxtestdso.so";
+#else
+#   error "Unknown compiler"
+#endif
+
 typedef sxx_int32_t (*sxx_test_dso_add_f)(sxx_int32_t n1, sxx_int32_t n2);
 
 sxx_bool_t sxx_tester_dso(sxx_test_suite_t *suite, sxx_int32_t argc, sxx_char_t *argv[])
@@ -7,7 +19,7 @@ sxx_bool_t sxx_tester_dso(sxx_test_suite_t *suite, sxx_int32_t argc, sxx_char_t 
     sxx_dso_t *dh = NULL;
     sxx_test_dso_add_f add_f = NULL;
 
-    sxx_assert_return_val(sxx_dso_create(sxx_test_suite_pool_get(suite), "./sxxtestdso.dll", &dh) == SXX_SUCCESS, SXX_FALSE);
+    sxx_assert_return_val(sxx_dso_create(sxx_test_suite_pool_get(suite), dlpath, &dh) == SXX_SUCCESS, SXX_FALSE);
 
     sxx_assert_return_val(sxx_dso_sym(dh, "sxx_test_dso_add", (sxx_dso_sym_t*)&add_f) == SXX_SUCCESS, SXX_FALSE);
 
